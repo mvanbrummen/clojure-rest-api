3fff8e8640f82ebabb9275916f68afaf9d977704
Michael Van Brummen <michaelvanbrummen@gmail.com>
2019-10-04 13:06:06 +1000

    Added swagger

--- src/snippet_rest_api/core.clj
+++ src/snippet_rest_api/core.clj
@@ -6,13 +6,41 @@
             [snippet-rest-api.repository :as repository]
             [schema.core :as s]))
 
+(s/defschema Snippet
+  {
+   :id          s/Int
+   :title       s/Str
+   :content     s/Str
+   :file_name   s/Str
+   :create_date s/Str
+   }
+  )
+
 (def db-connection (jdbc/get-datasource db-config/db-spec))
 
 (def app
   (api
+    :swagger
+    {
+     :ui   "/"
+     :spec "/swagger.json"
+     :data {
+            :info     {
+                       :title       "Snippet API"
+                       :description "API for code snippets"
+                       }
+            :tags     [{:name "api" :description "some apis"}]
+            :produces ["application/json"]
+            :consumes ["application/json"]
+            }
+     }
+
     (GET "/snippets" []
+      :summary "Returns all snippets"
       (ok (repository/snippets db-connection)))
+
     (GET "/snippets/:id" []
+      :summary "Returns a snippet for an ID"
       :path-params [id :- s/Int]
       (ok (repository/snippet db-connection id))
       )))
